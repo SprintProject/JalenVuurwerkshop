93703e5fb8a48db46500ac8687b501871e52adcd
scrub-masters <45385704+scrub-masters@users.noreply.github.com>
2018-12-05 13:57:49 +0100

    Vuurwerkshop 1.0 index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  require($DOCUMENT_ROOT . "navigation.php");
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheeter.css">
+	<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet'>
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="style.css" rel="stylesheet" type="text/css">
+<title>Jay</title>
+</head>
+<body>
+<h2>Welcome to the site!</h2>
+
+<div class="div1">
+<br><br><br><br><br>
+</div>
+
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheeter.css">
+	<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet'>
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+
+
+</head>
+<body>
+	<h1>Vuurwerk Shop</h1>
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="#about">About</a></li>
+  	</nav>
+</ul>
+
+<div class="footer">
+  <p6>Made by Jalen ©</p>
+</div>
+
+
+</body>
+<html>
